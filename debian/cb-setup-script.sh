f71591f92e67117652596b8f15af14372c515725
Matus Kmit <matus.kmit@zhdk.ch>
2013-11-08 20:25:59 +0100

    owncloud-client in setup script

--- debian/cb-setup-script.sh
+++ debian/cb-setup-script.sh
@@ -2,12 +2,11 @@
 
 # install owncloud ##################
 
-echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_7.0/ /' | sudo tee -a /etc/apt/sources.list.d/owncloud.list
-sudo aptitude update
-sudo aptitude install owncloud
-
+echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:devel/Debian_7.0/ /' | sudo tee -a /etc/apt/sources.list.d/owncloud-client.list
 wget http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_7.0/Release.key -P ~/downloads
 sudo apt-key add - < ~/downloads/Release.key
+sudo aptitude update
+sudo aptitude install owncloud-client
 
 ####################################
 
@@ -22,6 +21,12 @@ cd ~/stuff
 
 if [ ! -d /media/nas/Bhakti ]
 then
-        mkdir -p /media/nas/Bhakti
+        sudo mkdir -p /media/nas/Bhakti
         echo '192.168.1.3:/volume1/Bhakti     /media/nas/Bhakti      nfs    auto  0  0' | sudo tee -a /etc/fstab
+fi
+
+if [ ! -d /media/nas/Movies ]
+then
+        sudo mkdir -p /media/nas/Movies
+        echo '192.168.1.3:/volume1/Movies     /media/nas/Movies      nfs    auto  0  0' | sudo tee -a /etc/fstab
 fi
