191c893d7a7c76aaa5f326759df3d52a766f1cec
Matus Kmit <matus.kmit@zhdk.ch>
2013-11-08 17:19:54 +0100

    debian/cb-setup-script.sh

--- debian/cb-setup-script.sh
+++ debian/cb-setup-script.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_7.0/ /' >> /etc/apt/sources.list.d/owncloud.list
-sudo apt-get update
-sudo apt-get install owncloud
+# install owncloud ##################
+
+echo 'deb http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_7.0/ /' | sudo tee -a /etc/apt/sources.list.d/owncloud.list
+sudo aptitude update
+sudo aptitude install owncloud
 
 wget http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_7.0/Release.key -P ~/downloads
 sudo apt-key add - < ~/downloads/Release.key
+
+####################################
 
 if [ ! -d ~/stuff ]
 then
@@ -13,3 +17,11 @@ then
 fi
 
 cd ~/stuff
+
+# set mount point for nas in fstab ######################
+
+if [ ! -d /media/nas/Bhakti ]
+then
+        mkdir -p /media/nas/Bhakti
+        echo '192.168.1.3:/volume1/Bhakti     /media/nas/Bhakti      nfs    auto  0  0' | sudo tee -a /etc/fstab
+fi
